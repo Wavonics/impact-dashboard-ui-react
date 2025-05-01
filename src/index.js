1b574b276d3413670de996624d2fc04d5723ba87
Wavonics <nobodo@wavonics.com>
2025-04-30 23:32:53 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './components/App';
 import Assets from './components/Assets';
 import Contracts from './components/Contracts';
+import Projects from './components/Projects';
+import POs from './components/POs';
+import BudgetLines from './components/BudgetLines';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,6 +15,9 @@ root.render(
       <Route path="/" element={<App />} />
       <Route path="/assets" element={<Assets />} />
       <Route path="/contracts" element={<Contracts />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/po" element={<POs />} />
+      <Route path="/budget-lines" element={<BudgetLines />} />
     </Routes>
   </BrowserRouter>
 );
