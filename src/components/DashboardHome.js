1723b6558cceb145959c7ea8f95087e74876ad4f
Wavonics <nobodo@wavonics.com>
2025-05-03 18:36:22 -0400

    Create DashboardHome.js

---
+++ src/components/DashboardHome.js
@@ -0,0 +1,11 @@
+// components/DashboardHome.js
+import React from 'react';
+
+export default function DashboardHome() {
+  return (
+    <div>
+      <h1>Welcome to the IMPACT Dashboard</h1>
+      <p>Select a module from the navigation menu.</p>
+    </div>
+  );
+}
